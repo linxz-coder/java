f8dc850129eb6a55fcc7721d4f17bd7988f0e745
linxz-coder <234692261@qq.com>
2021-09-08 07:31:25 +0800

    Interface

--- Interface/src/academy/learnprogramming/Main.java
+++ Interface/src/academy/learnprogramming/Main.java
@@ -3,10 +3,15 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-        ITelephone timesPhone;
-        timesPhone = new DeskPhone(123456);
-        timesPhone.powerOn();
-        timesPhone.callPhone(123456);
-        timesPhone.answer();
+        ITelephone timsPhone;
+        timsPhone = new DeskPhone(123456);
+        timsPhone.powerOn();
+        timsPhone.callPhone(123456);
+        timsPhone.answer();
+
+        timsPhone = new MobilePhone(24565);
+        timsPhone.powerOn();
+        timsPhone.callPhone(24565);
+        timsPhone.answer();
     }
 }
